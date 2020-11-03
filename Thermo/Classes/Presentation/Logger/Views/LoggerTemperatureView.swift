dfbd642de8b8bb8a3c3509b361ef25871a8b1a47
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2020-11-04 00:28:33 +0300

    temperature logger change logic

--- Thermo/Classes/Presentation/Logger/Views/LoggerTemperatureView.swift
+++ Thermo/Classes/Presentation/Logger/Views/LoggerTemperatureView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class LoggerTemperatureView: GradientView {
     lazy var titleLabel = makeTitleLabel()
-    lazy var valueLabel = makeValueLabel()
+    lazy var temperaturePicker = makeTemperaturePicker()
     lazy var continueButton = makeContinueButton()
     
     var range: TemperatureRange? {
@@ -18,11 +18,9 @@ final class LoggerTemperatureView: GradientView {
         }
     }
     
-    var value: Double = 0 {
-        didSet {
-            update()
-        }
-    }
+    private(set) var value: Double = 0
+    
+    private var temperatures = [Double]()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,13 +34,50 @@ final class LoggerTemperatureView: GradientView {
     }
 }
 
+// MARK: UIPickerViewDelegate
+extension LoggerTemperatureView: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let attrs = TextAttributes()
+            .font(Fonts.OpenSans.bold(size: 68.scale))
+            .lineHeight(75.scale)
+            .textColor(UIColor.white)
+            .textAlignment(.center)
+        
+        let text = String(format: "%.1f", temperatures[row])
+            .attributed(with: attrs)
+        
+        let view = UILabel()
+        view.backgroundColor = UIColor.clear
+        view.attributedText = text
+        
+        updateColors(current: temperatures[row])
+        
+        return view
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        75.scale
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        value = temperatures[row]
+    }
+}
+
+// MARK: UIPickerViewDataSource
+extension LoggerTemperatureView: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        temperatures.count
+    }
+}
+
 // MARK: Private
 private extension LoggerTemperatureView {
     func configure() {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panAction(gesture:)))
-        isUserInteractionEnabled = true
-        addGestureRecognizer(panGesture)
-        
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 1, y: 1)
     }
@@ -50,62 +85,35 @@ private extension LoggerTemperatureView {
     func start() {
         value = range?.normal ?? 0
         
-        updateColors()
+        updateColors(current: value)
+        updatePicker()
     }
     
-    func update() {
-        let attrs = TextAttributes()
-            .font(Fonts.OpenSans.bold(size: 68.scale))
-            .textColor(UIColor.white)
-            .textAlignment(.center)
-        
-        valueLabel.attributedText = String(format: "%.1f", value).attributed(with: attrs)
-    }
-    
-    @objc
-    func panAction(gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: self)
-        
-        switch gesture.state {
-        case .changed:
-            calculateTemperature(y: translation.y)
-            updateColors()
-        default:
-            break
-        }
-    }
-    
-    func calculateTemperature(y: CGFloat) {
+    func updatePicker() {
         guard let range = self.range else {
             return
         }
         
-        let step = Double(UIScreen.main.bounds.height / 2)
-        
-        let inversY = y * CGFloat(-1)
-        
-        let upper = Double(inversY) / step
-        
-        var newValue = value + upper
-        
-        if newValue < range.min {
-            newValue = range.min
-        } else if newValue > range.max {
-            newValue = range.max
+        for value in stride(from: range.min, to: range.max + 0.1, by: 0.1) {
+            temperatures.append(value)
         }
         
-        value = newValue
+        temperaturePicker.reloadAllComponents()
+        
+        if let initialIndex = temperatures.firstIndex(of: range.normal) {
+            temperaturePicker.selectRow(initialIndex, inComponent: 0, animated: false)
+        }
     }
     
-    func updateColors() {
+    func updateColors(current: Double) {
         guard let range = self.range else {
             return
         }
         
-        let topRed = calculteColor(min: 108, max: 255, range: range) + 108
-        let bottomRed = calculteColor(min: 53, max: 255, range: range) + 108
-        let topBlue = 255 - calculteColor(min: 108, max: 255, range: range)
-        let bottomBlue = 255 - calculteColor(min: 53, max: 255, range: range)
+        let topRed = calculateColor(min: 108, max: 255, range: range, current: current) + 108
+        let bottomRed = calculateColor(min: 53, max: 255, range: range, current: current) + 108
+        let topBlue = 255 - calculateColor(min: 108, max: 255, range: range, current: current)
+        let bottomBlue = 255 - calculateColor(min: 53, max: 255, range: range, current: current)
         
         gradientLayer.colors = [
             UIColor(integralRed: topRed, green: 0, blue: topBlue).cgColor,
@@ -113,11 +121,11 @@ private extension LoggerTemperatureView {
         ]
     }
     
-    func calculteColor(min: CGFloat, max: CGFloat, range: TemperatureRange) -> CGFloat {
+    func calculateColor(min: CGFloat, max: CGFloat, range: TemperatureRange, current: Double) -> CGFloat {
         let temperatureDiff = CGFloat(range.max - range.min)
         let colorsDiff = max - min
         let colorUnitForOneDegree = colorsDiff / temperatureDiff
-        return CGFloat(value - range.min) * colorUnitForOneDegree
+        return CGFloat(current - range.min) * colorUnitForOneDegree
     }
 }
 
@@ -131,8 +139,9 @@ private extension LoggerTemperatureView {
         ])
         
         NSLayoutConstraint.activate([
-            valueLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            valueLabel.topAnchor.constraint(equalTo: topAnchor, constant: 450.scale)
+            temperaturePicker.centerXAnchor.constraint(equalTo: centerXAnchor),
+            temperaturePicker.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50.scale),
+            temperaturePicker.bottomAnchor.constraint(equalTo: continueButton.topAnchor, constant: -100.scale)
         ])
         
         NSLayoutConstraint.activate([
@@ -161,8 +170,11 @@ private extension LoggerTemperatureView {
         return view
     }
     
-    func makeValueLabel() -> UILabel {
-        let view = UILabel()
+    func makeTemperaturePicker() -> UIPickerView {
+        let view = UIPickerView()
+        view.dataSource = self
+        view.delegate = self
+        view.backgroundColor = UIColor.clear
         view.translatesAutoresizingMaskIntoConstraints = false
         addSubview(view)
         return view
